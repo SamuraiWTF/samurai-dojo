6ac7273142e3d450ab4980c7ae75e05146fc977e
Kevin Johnson <kevin@secureideas.com>
2019-04-27 11:03:11 -0400

    updated

--- src/basic/footer.php
+++ src/basic/footer.php
@@ -8,8 +8,9 @@ include 'closedb.inc';
 <br>
 <hr>
 <div class="row" id="footerbar">
-    Copyright 2012 Justin Searle<br/>
-Samurai Dojo-Basic is a <a href="http://www.samurai-wtf.org" target="_blank">SamuraiWTF</a> Project.
+    Originally Created by Justin Searle<br/>
+    Maintained by the SamuraiWTF team<br />
+Samurai Dojo-Basic is a <a href="http://github.com/SmauraiWTF" target="_blank">SamuraiWTF</a> Project.
 </div>
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
