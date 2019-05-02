869c9888b75372dadb6ca5edde9a0e9e29f10c1d
Jason <jason.gillam@gmail.com>
2019-05-02 15:11:14 -0400

    Added basic login page.

--- src/helpdesk/index.php
+++ src/helpdesk/index.php
@@ -1,20 +1,5 @@
-<?php include 'language.inc'; ?>
+<?php include 'header.inc'; ?>
 
-<!doctype html>
-
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="SamuraiWTF">
-
-    <title><? echo INDEX_TITLE;?></title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="/css/product.css" rel="stylesheet">
   </head>
 
@@ -25,7 +10,7 @@
       <!--  <a class="py-2" href="#">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg> 
         </a> -->
-        <a class="py-2 d-none d-md-inline-block" href="#"><? echo MENU_TXT_LOGIN_CUSTOMER;?></a>
+        <a class="py-2 d-none d-md-inline-block" href="/login.php"><? echo MENU_TXT_LOGIN_CUSTOMER;?></a>
         <a class="py-2 d-none d-md-inline-block" href="#"><? echo MENU_TXT_LOGIN_EMPLOYEE;?></a>
         <a class="py-2 d-none d-md-inline-block" href="#"><? echo MENU_TXT_FAQ;?></a>
       </div>
